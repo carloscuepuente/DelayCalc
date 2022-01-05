fb0716ded17d078d040bb489cb661473b22e8b8b
Carlos Cue <46687878+carloscuepuente@users.noreply.github.com>
2022-01-05 23:00:30 +0100

    Add files via upload

--- CalculadoraDelay.js
+++ CalculadoraDelay.js
@@ -7,13 +7,17 @@ let DesConACU;
 let IniFuel;
 let FinFuel;
 let OnBlocks;
-let PreEmbarque
+let PreEmbarque;
+let PreEmbarqueStop;
+let PreEmbarqueStart;
 let IniDesEmbarque;
 let FinDesEmbarque;
+let PasajeIn;
 let IniArrPMR;
 let FinArrPMR;
 let IniEmbarque;
 let FinEmbarque;
+let PasajeOut;
 let IniDepPMR;
 let FinDepPMR;
 let CierreEmbarque;
@@ -54,6 +58,7 @@ const getDuration = function (time1, time2) {
     }
 
 }
+
 document.querySelector("button").disabled = true
 const delayTypeSelector = document.querySelector("#DelayType")
 
@@ -67,14 +72,33 @@ delayTypeSelector.addEventListener("change", function (event) {
 
         for (let i = 0; i < noBasico.length; i++) {
             noBasico[i].disabled = true
+            noBasico[i].classList.add("hide")
         }
     }
 
+    if (delayCode === "86") {
+        document.querySelector("button").disabled = false;
+        const PreEmbarqueOptional = document.querySelectorAll(".PreEmbarqueOptional")
+        const noBasico = document.querySelectorAll(".noBasico")
+
+        for (let i = 0; i < PreEmbarqueOptional.length; i++) {
+            PreEmbarqueOptional[i].classList.toggle("hide")
+        }
+
+        for (let i = 0; i < noBasico.length; i++) {
+            noBasico[i].disabled = true
+            noBasico[i].classList.add("hide")
+        }
+
+    }
+
+    // ayuda a "reiniciar los inputs considerar hacer una funcion que lo haga"
     if (delayCode === "inicial") {
 
-        const allInputs = document.querySelectorAll("input")
-        for (let i = 0; i < allInputs.length; i++) {
-            allInputs[i].disabled = false;
+        const noBasico = document.querySelectorAll(".noBasico")
+        for (let i = 0; i < noBasico.length; i++) {
+            noBasico[i].disabled = false;
+            noBasico[i].classList.remove("hide")
         }
         document.querySelector("button").disabled = true
     }
@@ -84,31 +108,64 @@ delayTypeSelector.addEventListener("change", function (event) {
 delayCalcForm.addEventListener("submit", function (event) {
     event.preventDefault();
 
+    const delayCode = event.target.DelayType.value
+
     STA = formaElements.STA.value
     STD = formaElements.STD.value
     OnBlocks = formaElements.OnBlocks.value
     PreEmbarque = formaElements.PreEmbarque.value
     IniDesEmbarque = formaElements.IniDesEmbarque.value
     FinDesEmbarque = formaElements.FinDesEmbarque.value
+    PasajeIn = formaElements.PasajeIn.value
     IniEmbarque = formaElements.IniEmbarque.value
     FinEmbarque = formaElements.FinEmbarque.value
+    PasajeOut = formaElements.PasajeOut.value
     CierreEmbarque = formaElements.CierreEmbarque.value
     EntregaLID = formaElements.EntregaLID.value
     CierrePuertasAC = formaElements.CierrePuertasAC.value
     OffBlocks = formaElements.OffBlocks.value
 
-    const divContainer = document.querySelector("#divResult")
-    const newParagraph = document.createElement("p");
-    newParagraph.innerText = `STA ${parseTimeAndFormat(STA)}, STD ${parseTimeAndFormat(STD)} ATA STD ${getDuration(OnBlocks, STD)}
-    1st pax off STD ${getDuration(IniDesEmbarque, STD)} / Last pax off STD ${getDuration(FinDesEmbarque, STD)} / total time: ${getDuration(FinDesEmbarque, IniDesEmbarque)} min
-    1st pax on STD ${getDuration(IniEmbarque, STD)} / Last pax on at STD ${getDuration(FinEmbarque, STD)} / total time: ${getDuration(FinEmbarque, IniEmbarque)} min
-    TOB count in & out {insertTOB} / {insertTOB}
-    1st pax scanned STD ${getDuration(PreEmbarque, STD)} / last pax scanned at STD ${getDuration(CierreEmbarque, STD)} / total time: ${getDuration(CierreEmbarque, PreEmbarque)} min
-    LID delivered at STD ${getDuration(EntregaLID, STD)}
-    A/C doors closed at STD ${getDuration(CierrePuertasAC, STD)}
-    On-blox STD ${getDuration(OnBlocks, STD)} / Off-blox STD ${getDuration(OffBlocks, STD)} / total time: ${getDuration(OffBlocks, OnBlocks)} min `
+    if (delayCode === "Basico") {
 
-    divContainer.appendChild(newParagraph);
+        const divContainer = document.querySelector("#divResult")
+        const newParagraph = document.createElement("p");
+        newParagraph.innerText = `STA ${parseTimeAndFormat(STA)}, STD ${parseTimeAndFormat(STD)} ATA STD ${getDuration(OnBlocks, STD)}
+        1st pax off STD ${getDuration(IniDesEmbarque, STD)} / Last pax off STD ${getDuration(FinDesEmbarque, STD)} / total time: ${getDuration(FinDesEmbarque, IniDesEmbarque)} min
+        1st pax on STD ${getDuration(IniEmbarque, STD)} / Last pax on at STD ${getDuration(FinEmbarque, STD)} / total time: ${getDuration(FinEmbarque, IniEmbarque)} min
+        TOB count in & out ${PasajeIn} / ${PasajeOut}
+        1st pax scanned STD ${getDuration(PreEmbarque, STD)} / last pax scanned at STD ${getDuration(CierreEmbarque, STD)} / total time: ${getDuration(CierreEmbarque, PreEmbarque)} min
+        LID delivered at STD ${getDuration(EntregaLID, STD)}
+        A/C doors closed at STD ${getDuration(CierrePuertasAC, STD)}
+        On-blox STD ${getDuration(OnBlocks, STD)} / Off-blox STD ${getDuration(OffBlocks, STD)} / total time: ${getDuration(OffBlocks, OnBlocks)} min `
+
+        divContainer.appendChild(newParagraph);
+    };
+
+
+    if (delayCode === "86") {
+
+        PreEmbarqueStop = formaElements.PreEmbarqueStop.value
+        PreEmbarqueStart = formaElements.PreEmbarqueStart.value
+        const divContainer = document.querySelector("#divResult")
+        const newParagraph = document.createElement("p");
+        newParagraph.innerText = `
+        STA ${parseTimeAndFormat(STA)}, STD ${parseTimeAndFormat(STD)} ATA STD ${getDuration(OnBlocks, STD)}
+        1st pax off STD ${getDuration(IniDesEmbarque, STD)} / Last pax off STD ${getDuration(FinDesEmbarque, STD)} / total time: ${getDuration(FinDesEmbarque, IniDesEmbarque)} min
+        1st pax on STD ${getDuration(IniEmbarque, STD)} / Last pax on at STD ${getDuration(FinEmbarque, STD)} / total time: ${getDuration(FinEmbarque, IniEmbarque)} min
+        TOB count in & out ${PasajeIn} / ${PasajeOut}
+        1st pax scanned STD ${getDuration(PreEmbarque, STD)} / last pax scanned at STD ${getDuration(CierreEmbarque, STD)} / total time: ${getDuration(CierreEmbarque, PreEmbarque)} min
+        LID delivered at STD ${getDuration(EntregaLID, STD)}
+        A/C doors closed at STD ${getDuration(CierrePuertasAC, STD)}
+        On-blox STD ${getDuration(OnBlocks, STD)} / Off-blox STD ${getDuration(OffBlocks, STD)} / total time: ${getDuration(OffBlocks, OnBlocks)} min 
+        
+        Reduced capacity of pre-boarding area due to COVID restrictions
+        1st pax scanned STD ${getDuration(PreEmbarque, STD)} / preboarding stopped STD ${getDuration(PreEmbarqueStop, STD)} / total time ${getDuration(PreEmbarqueStop, PreEmbarque)}
+        Boarding re-started STD ${getDuration(PreEmbarqueStart, STD)} / last pax scanned at STD ${getDuration(CierreEmbarque, STD)} / total time ${getDuration(PreEmbarqueStart, CierreEmbarque)}`
+        divContainer.appendChild(newParagraph);
+    }
+
+
+
 
     // STA 11:00, STD 11:30 ATA STD+0
     // 1st pax off STD-28 / Last pax off STD-19 / total time: 9 min
